b90df354ce3851789306a309fcd910b585c040df
Mika312 <michaelpeltier31284@gmail.com>
2019-06-27 16:11:55 +0200

    Mise en place Base de données + Schema Movies + test PostMan des routes/Bdd

---
+++ models/movies.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+var movieSchema = mongoose.Schema({
+title: String,
+overview : String,
+poster_path : String,
+idMovieDB : Number,
+});
+module.exports = mongoose.model('movies', movieSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var movieModel = require('../models/movies');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,22 +10,45 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/movies', function(req, res, next) {
-  res.json({ result: true });
+  request("https://api.themoviedb.org/3/discover/movie?api_key=f464c3ad43a52a8cd59f4f1e13f28695&language=fr&sort_by=popularity.desc&include_adult=false&include_video=false&page=1", function(error, response, body) {
+    body=JSON.parse(body)
+  res.json({ result: true, body});
+
+  });
 });
 
 
 router.get('/mymovies', function(req, res, next) {
-  res.json({ result: true });
+  movieModel.find(
+    function (err, movies) {
+      res.json({ result: true, movies});
+    })
 });
 
 
 router.post('/mymovies', function(req, res, next) {
-  res.json({ result: true });
+
+  var newMovie = new movieModel ({
+    title: req.body.title,
+    overview : req.body.overview,
+    poster_path : req.body.poster_path,
+    idMovieDB : req.body._id,
+   });
+
+   newMovie.save(
+    function (error, movie) {
+    res.json({ result: true, movie });
+  });
 });
 
 
-router.delete('/mymovies/:movieId', function(req, res, next) {
-  res.json({ result: true });
+router.delete('/mymovies/:movieId', function(req, res, next) { 
+  movieModel.deleteOne(
+    { idMovieDB: req.params.movieId}, 
+    function(error) {
+      res.json({ result: true});
+
+    });
 });
 
 
